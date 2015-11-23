d78a61edc4828b0f3d9ab036f5567eac4817c407
noear <noear@live.cn>
2015-11-23 15:48:06 +0800

    第一次提交代码

---
+++ AsyncHttpClient.UWP/AsyncHttpClient.UWP/AsyncHttpClient.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+using Windows.Web.Http.Filters;
+
+namespace Noear.UWP.Http
+{
+    public class AsyncHttpClient
+    {
+        private string _url;
+        private Dictionary<string, string> _headers;
+        private Dictionary<string, string> _cookies;
+        
+        public AsyncHttpClient Url(string url) {
+            _url = url;
+            return this;
+        }
+
+        public AsyncHttpClient Header(string name, string value) {
+            if (_headers == null) {
+                _headers = new Dictionary<string, string>();
+            }
+            _headers.Add(name, value);
+            
+            return this;
+        }
+
+        public AsyncHttpClient Cookie(string name, string value) {
+            if (_cookies == null) {
+                _cookies = new Dictionary<string, string>();
+            }
+            _cookies.Add(name, value);
+
+            return this;
+        }
+
+        public async Task<AsyncHttpResponse> Get(string encoding) {
+            if (string.IsNullOrEmpty(encoding)) {
+                encoding = "UTF-8";
+            }
+
+            var client = DoBuildHttpClient(encoding);
+
+            using (var rsp = await client.GetAsync(new Uri(_url))) {
+                return new AsyncHttpResponse(rsp, encoding);
+            }
+        }
+        
+        public async Task<AsyncHttpResponse> Post(string encoding, Dictionary<string, string> args) {
+            if (string.IsNullOrEmpty(encoding)) {
+                encoding = "UTF-8";
+            }
+
+            var client = DoBuildHttpClient( encoding);
+
+            var postData = new HttpFormUrlEncodedContent(args);
+            
+            using (var rsp = await client.PostAsync(new Uri(_url), postData)) {
+                return new AsyncHttpResponse(rsp, encoding);
+            }
+        }
+
+        private HttpClient DoBuildHttpClient(string encoding) {
+            HttpClient client = null;
+            if (_cookies != null) {
+                var bpf = new HttpBaseProtocolFilter();
+                var cm = bpf.CookieManager;
+                foreach (var kv in _cookies) {
+                    var cookie = new HttpCookie(kv.Key, _url, "/") { Value = kv.Value };
+                    cm.SetCookie(cookie);
+                }
+
+                client = new HttpClient(bpf);
+            }
+            else {
+                client = new HttpClient();
+            }
+
+            client.DefaultRequestHeaders.Add("Encoding", encoding);
+
+            if (_headers != null) {
+                foreach (var kv in _headers) {
+                    client.DefaultRequestHeaders.Add(kv.Key, kv.Value);
+                }
+            }
+
+            return client;
+        }
+    }
+}
---
+++ AsyncHttpClient.UWP/AsyncHttpClient.UWP/AsyncHttpResponse.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.Web.Http;
+
+namespace Noear.UWP.Http {
+    public class AsyncHttpResponse :IDisposable{
+        protected IBuffer data { get; private set; }
+
+        public Dictionary<string, string> Headers { get; private set; }
+        public string Cookies { get; private set; }
+        public HttpStatusCode StatusCode { get; private set; }
+        public Encoding Encoding { get; private set; }
+
+        internal AsyncHttpResponse(HttpResponseMessage rsp, string encoding) {
+            this.StatusCode = rsp.StatusCode;
+            this.Encoding   = Encoding.GetEncoding(encoding);
+            this.Headers    = new Dictionary<string, string>();
+            this.Cookies    = null;
+            init(rsp);
+        }
+
+        protected async void  init(HttpResponseMessage rsp) {
+            if (rsp.StatusCode == HttpStatusCode.Ok) {
+                data = await rsp.Content.ReadAsBufferAsync();
+            }
+
+            if (rsp.Headers != null) {
+                foreach (var kv in rsp.Headers) {
+                    if ("Set-Cookie".Equals(kv.Key)) {
+                        Cookies = kv.Value;
+                    }
+
+                    Headers.Add(kv.Key, kv.Value);
+                }
+            }
+        }
+
+        public virtual IBuffer getBuffer() {
+            return data;
+        }
+
+        public virtual byte[] GetBytes() {
+            return  WindowsRuntimeBufferExtensions.ToArray(data, 0, (int)data.Length);
+        }
+
+        public virtual string GetString() {
+            var bytes = GetBytes();
+            return  this.Encoding.GetString(bytes);
+        }
+
+        public void Dispose() {
+            data = null;
+            Headers = null;
+            Cookies = null;
+            Encoding = null;
+        }
+    }
+}
